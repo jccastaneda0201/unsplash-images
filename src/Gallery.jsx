b8ff0f19419f2efadd324bd0259adf0a84bdf6b9
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-30 11:44:42 +0200

    feat: add components and styling setup axios

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const URL =
+  'https://api.unsplash.com/search/photos?client_id=hFd51wRNtDqxT14P5yaW89PjV0Xy-N1rK57NjgWkrJE&query=dog';
 
 const Gallery = () => {
-  return <div>Gallery</div>;
-};
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(URL);
+      return result.data;
+    },
+  });
+  console.log(response);
 
+  return <h2>Gallery</h2>;
+};
 export default Gallery;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { AppProvider } from './context.jsx';
 import './index.css';
 import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 );
